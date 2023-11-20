d3142bd6a635ba80e4eb89ae11ff76cea7c928c8
savageafrika <113559289+savageafrika@users.noreply.github.com>
2023-11-20 07:25:46 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,491 @@
+
+
+
+
+ 
+
+   //Questions array
+
+   let questions = [
+
+    {
+
+         number:1,
+
+         question: "Who defined tragedy as an imitation of an action that is serious",
+
+         answer : "Aristotle",
+
+         options : [
+
+                          "William Wordsworth",
+
+                          "Aristotle",
+
+                          "Blake Shelton",
+
+                          "Pythagoras"
+
+                        ]
+
+    
+
+    },
+
+    {
+
+         number:2,
+
+         question: "The tragic heroes hamartia or defects should be the outcome of",
+
+         answer : "Error in judgement",
+
+         options : [
+
+                          "Conflict",
+
+                          "Error in judgement",
+
+                          "Personal development issues",
+
+                          "Bad character"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:3,
+
+         question: "Tragic flaw in Greek is called",
+
+         answer : "Hubris",
+
+         options : [
+
+                          "Hubris",
+
+                          "Hamartia",
+
+                          "Haiku",
+
+                          "Dran"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:4,
+
+         question: "___ is achieved through feelings of sorrow and terror",
+
+         answer : "Tragic catharsis",
+
+         options : [
+
+                          "Tragic fall",
+
+                          "Tragic catharsis",
+
+                          "Conflict",
+
+                          "Denouement"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:5,
+
+         question: "___ is a weapon used to expose ignorance and deception",
+
+         answer : "Humor",
+
+         options : [
+
+                          "Humor",
+
+                          "Plot",
+
+                          "Situational irony",
+
+                          "Comic catharsis"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:6,
+
+         question: "The primary character of a melodrama is",
+
+         answer : "Villain",
+
+         options : [
+
+                          "Hero",
+
+                          "Protagonist",
+
+                          "Villain",
+
+                          "Heroine"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:7,
+
+         question: "The main goal of melodrama is to",
+
+         answer : "Entertain",
+
+         options : [
+
+                          "Amuse",
+
+                          "Criticize",
+
+                          "Elicit emotions",
+
+                          "Entertain"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:8,
+
+         question: "Aristotle claimed that the plot is __",
+
+         answer : "Life and soul of tragedy",
+
+         options : [
+
+                          "A tool for tragic catharsis",
+
+                          "Life and soul of tragedy",
+
+                          "Entire storyline of a play",
+
+                          "Basic structure of drama"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:9,
+
+         question: "The two key elements of an enjoyable story line is",
+
+         answer : "Suspense and surprise",
+
+         options : [
+
+                          "Suspense and surprise",
+
+                          "Plot and setting",
+
+                          "Character and action",
+
+                          "Comedy and tragedy"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:10,
+
+         question: "___ is a technique used in exposition part of a plot, when the writer uses subtle indications to get the audience ready for upcoming events",
+
+         answer : "Foreshadowing",
+
+         options : [
+
+                          "Foreshadowing",
+
+                          "Soliloquy",
+
+                          "Catharsis",
+
+                          "Flashback"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:11,
+
+         question: "In tragedy, denouement is often called the ___",
+
+         answer : "Catastrophe",
+
+         options : [
+
+                          "Catastrophe",
+
+                          "Falling action",
+
+                          "Resolution",
+
+                          "End point"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:12,
+
+         question: "A well-known character is called",
+
+         answer : "Round character",
+
+         options : [
+
+                          "Round character",
+
+                          "Flat character",
+
+                          "Static character",
+
+                          "Dynamic character"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:13,
+
+         question: "A character that lacks depth and is usually one-dimensional is called",
+
+         answer : "Flat character",
+
+         options : [
+
+                          "Flat character",
+
+                          "Round character",
+
+                          "Static character",
+
+                          "Dynamic character"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:14,
+
+         question: "The character that goes through changes in a story is called",
+
+         answer : "Dynamic character",
+
+         options : [
+
+                          "Round character",
+
+                          "Flat character",
+
+                          "Dynamic character",
+
+                          "Static character"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:15,
+
+         question: "__ character serve as contrast by refusing to change throughout the entire story",
+
+         answer : "Static character",
+
+         options : [
+
+                          "Dynamic character",
+
+                          "Static character",
+
+                          "Flat character",
+
+                          "Round character"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:16,
+
+         question: "Which of these is a component of the dramatic character's personality",
+
+         answer : "Thought",
+
+         options : [
+
+                          "Soliloquy",
+
+                          "Catharsis",
+
+                          "Thought",
+
+                          "Dialogue"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:17,
+
+         question: "All of a play's visual components fall under __ category",
+
+         answer : "Spectacle",
+
+         options : [
+
+                          "Props",
+
+                          "Spectacle",
+
+                          "Stageries",
+
+                          "Prompts"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:18,
+
+         question: "A drama condensed into a single episode delivered to a person or group in a one-sided interaction is known as",
+
+         answer : "Monologue",
+
+         options : [
+
+                          "Interlude",
+
+                          "Prologue",
+
+                          "Aside",
+
+                          "Monologue"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:19,
+
+         question: "The line of dialogue delivered by a character to the play’s audience is known as",
+
+         answer : "Aside",
+
+         options : [
+
+                          "Monologue",
+
+                          "Aside",
+
+                          "Interlude",
+
+                          "Soliloquy"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:20,
+
+         question: "The goal of literary appreciation is __",
+
+         answer : "To increase the value of written works",
+
+         options : [
+
+                          "To increase the value of written works",
+
+                          "To prescribe methods of literary writing",
+
+                          "To organise written works",
+
+                          "To analyse written works"  
+
+                          ]
+
+    
+
+    }
+
+     ];   
